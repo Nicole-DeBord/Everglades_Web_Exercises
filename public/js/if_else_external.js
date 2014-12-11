ec04331d44dd4d9fc4b3014024b1bae57dffbca6
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-11 15:33:16 -0600

    Changed console log output

--- public/js/if_else_external.js
+++ public/js/if_else_external.js
@@ -17,22 +17,22 @@ var favorite = 'indigo';
 // todo: In the else, log: I do not know anything by that color.
 
 if (color == 'red') {
-	console.log('Red is the color of rubies');
+	console.log('Red is the color of garnet.');
 } else if (color == 'orange') {
-	console.log('Orange is the color of citrines');
+	console.log('Orange is the color of citrine.');
 } else if  (color == 'yellow') {
-	console.log('Yellow is the color of scapolite');
+	console.log('Yellow is the color of scapolite.');
 } else if (color == 'green') {
-	console.log('Green is the color of malachite');
+	console.log('Green is the color of malachite.');
 } else if (color == 'blue') {
-	console.log('Blue is the color of amazonite');
+	console.log('Blue is the color of amazonite.');
 } else {
-	console.log('I can think of gemstones this color, but I\'m not going to tell you. Sorry to be like that.');
+	console.log('I can think of gemstones this color, but I\'m not going to tell you. Sorry to be like that. Embrace the mystery.');
 }
 
 // todo: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
 
-(color == favorite) ? console.log('There\'s a fifty percent chance this color is my favorite!') : console.log('This color is just okay.');
+(color == favorite) ? console.log('Hey, you found it - ' + color + ' is my favorite color!') : console.log('I think ' + color + ' is just okay.');
 
 
