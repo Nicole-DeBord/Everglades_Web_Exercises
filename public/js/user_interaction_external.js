c923b03613143920ee9f140d19db45c4a0041adf
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-12 15:13:20 -0600

    Added a user interaction exercise using alert, confirmation, and prompt with external JS

---
+++ public/js/user_interaction_external.js
@@ -0,0 +1,19 @@
+// User Input, JavaScript Style
+
+// todo: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+var userName = prompt('Hey buddy. Let\'s be friends. What\'s your name?');
+
+// todo: Show an alert message that welcomes the user based on their input.
+
+alert('Hiya ' + userName + '! Since we\'re getting friendly and all, I\'ve got this burning question...');
+
+// todo: Ask the user if they like pizza.
+//       Based on their answer tell show a creative alert message.
+
+var pizza = confirm('Do you like pizza?');
+
+var likesPizza = (pizza == true) ? alert('Me too bro! Pizza\'s the manna of enlightened beings.') : alert('That\'s cool bro, I respect your choices. Maybe you\'ll meet the right pizza someday.');
+
+ 
