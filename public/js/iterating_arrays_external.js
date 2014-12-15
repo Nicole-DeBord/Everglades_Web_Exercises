97daae0a0b9183d313448000f250d2ab3c35b1b7
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-15 10:59:34 -0600

    Added an array iteration exercise using external JS

---
+++ public/js/iterating_arrays_external.js
@@ -0,0 +1,17 @@
+// todo:
+// Create an array of 4 names using literal array notation, in a variable called 'names'.
+
+var names = ['War', 'Famine', 'Pestilence', 'Death'];
+
+// todo:
+// Create a log statement that will log the number of elements in the names array.
+
+console.log('There are ' + names.length + ' riders of the Apocalypse:');
+
+// todo:
+// Create log statements that will print each of the names array elements individually.
+
+console.log('Rider one is '   + names[0]);
+console.log('Rider two is '   + names[1]);
+console.log('Rider three is ' + names[2]);
+console.log('Rider four is '  + names[3]);
