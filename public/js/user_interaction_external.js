f89417fc29ae92d598ab1e1c165ed29dab90e85f
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-17 09:40:51 -0600

    Updated to request input if none given

--- public/js/user_interaction_external.js
+++ public/js/user_interaction_external.js
@@ -1,19 +1,23 @@
 // User Input, JavaScript Style
+ 
+// Ask the user for their name; keeping asking if no input is given.
 
-// todo: Ask the user for their name.
-//       Keep asking if an empty input is provided.
+do {
 
 var userName = prompt('Hey buddy. Let\'s be friends. What\'s your name?');
 
-// todo: Show an alert message that welcomes the user based on their input.
+} while (userName == '' || userName == ' ' || userName == null);
+
+// Show an alert message that welcomes the user based on their input.
 
 alert('Hiya ' + userName + '! Since we\'re getting friendly and all, I\'ve got this burning question...');
 
-// todo: Ask the user if they like pizza.
-//       Based on their answer tell show a creative alert message.
+// Ask the user if they like pizza.
 
 var pizza = confirm('Do you like pizza?');
 
-var likesPizza = (pizza == true) ? alert('Me too bro! Pizza\'s the manna of enlightened beings.') : alert('That\'s cool bro, I respect your choices. Maybe you\'ll meet the right pizza someday.');
+// Based on their answer show a creative alert message.
 
- 
+var likesPizza = (pizza == true) ? alert
+('Me too bro! Pizza\'s the manna of enlightened beings.') : 
+alert('That\'s cool bro, I respect your choices. Maybe you\'ll meet the right pizza someday.');
